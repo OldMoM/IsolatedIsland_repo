d59d0a76c06115be10f8dab67a00d91ed5118bb3
cecilycao <caoye1218@gmail.com>
2021-02-24 23:42:41 -0800

    DialogueSystem

---
+++ Assets/Scripts/CaoyeScripts/Dialogue/DialogueTest.cs
@@ -0,0 +1,24 @@
+﻿using Caoye;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTest : MonoBehaviour
+{
+    public bool isActive;
+    IDialogSystem testDialog;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        testDialog = FindObjectOfType<DialogueSystem>();
+        isActive = testDialog.isActive;
+        testDialog.StartDialog("3");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isActive = testDialog.isActive;
+    }
+}
