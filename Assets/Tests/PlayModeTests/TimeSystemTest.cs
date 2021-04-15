4505983446af77820d9887c1ad06fc8b5f95c3fc
OldMoM <1090347251@qq.com>
2021-04-15 13:46:59 +0800

    Fix:时间系统
    修复BUG：开始游戏后，时间系统不向外广播OnDayStart事件

---
+++ Assets/Tests/PlayModeTests/TimeSystemTest.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using Peixi;
+using UniRx;
+
+namespace Tests
+{
+    public class TimeSystemTest
+    {
+        [UnityTest]
+        public IEnumerator OnDayStart_GameStart_ReceiveInt1()
+        {
+            var timeSystem = new GameObject().AddComponent<TimeSystem>();
+
+            timeSystem.onDayStart
+                .Subscribe(x =>
+                {
+                    
+                    Debug.Log("start first day");
+                    Assert.AreEqual(1, x);
+                });
+
+            yield return null;
+        }
+    }
+}
