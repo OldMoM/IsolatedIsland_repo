8cc7a2f6c4d1b0f008c277fd43c2f8f7b83dacd2
fourdimension <37730913+fourdimension@users.noreply.github.com>
2021-04-16 16:35:26 -0700

    Click Esc to open Quit canvas

---
+++ Assets/Scripts/SiweiScripts/QuitGame.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitGame : MonoBehaviour
+{
+    [HideInInspector]
+    public GameObject quitGameObj;
+    private void Awake()
+    {
+        quitGameObj = this.transform.Find("QuitPanel").gameObject;
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            quitGameObj.SetActive(!quitGameObj.activeSelf);
+        }
+    }
+
+    public void Quit()
+    {
+        Debug.Log("Click Quit button");
+        Application.Quit();
+    }
+
+    public void Cancel()
+    {
+        Debug.Log("Click Cancel button");
+        quitGameObj.SetActive(false);
+    }
+}
