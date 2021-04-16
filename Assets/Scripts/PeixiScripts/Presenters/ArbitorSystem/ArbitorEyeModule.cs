db9662da20da35f0bc4b69e23425f9a786783087
OldMoM <1090347251@qq.com>
2021-04-16 12:46:39 +0800

    Fix:零星文本未英化
    @ShowMessage.cs

--- Assets/Scripts/PeixiScripts/Presenters/ArbitorSystem/ArbitorEyeModule.cs
+++ Assets/Scripts/PeixiScripts/Presenters/ArbitorSystem/ArbitorEyeModule.cs
@@ -96,7 +96,7 @@ namespace Peixi
         public ArbitorEyeModule() { }
         public void OnPlayerTouch(BaseItem item)
         {
-            ShowMessage.singlton.Message("按下互动键收集资源");
+            ShowMessage.singlton.Message("Press E to collect resource");
             AddItemToPendingQuene(item);
         }
         public void OnPlayerUnTouch(BaseItem item)
